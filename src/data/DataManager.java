0632dafdb0368ae83cc8301a00aa2a4f25776a9c
elntousi <e.ntousi@edu.bbq.de>
2025-08-21 12:50:33 +0200

    Datenbankverbindung implementiert: Verbindung zur DB hergestellt und Thema-Speicherung ergänzen.

--- src/data/DataManager.java
+++ src/data/DataManager.java
@@ -1,17 +1,47 @@
 package data;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
+
+
 
 public class DataManager {
 	
 	public String saveThema(ThemaObject thema) {
-		// Hier könnte Code zum Speichern des Themas in einer Datenbank oder Datei
-		// stehen
-		// Aktuell wird nur eine Bestätigung zurückgegeben
+		
+		Connection connection= null;
+		try {
+			connection = DriverManager.getConnection(URL, USER, PASSWORD);
+			String command = "INSERT INTO themen (titel, info) VALUES (?, ?)";
+			
+			PreparedStatement stmt = connection.prepareStatement(
+				    command, Statement.RETURN_GENERATED_KEYS
+				);
+
+			stmt.setString(1, thema.getTitel());
+			stmt.setString(2, thema.getInfo());
+			stmt.executeUpdate();
+			
+			ResultSet rs = stmt.getGeneratedKeys();
+			if (rs.next()) {
+				thema.setId(rs.getInt(1)); // Setze die generierte ID zurück ins Thema-Objekt
+			}
+			
+			stmt.close();	
+			
+			} catch (SQLException e) {
+				e.printStackTrace();
+		}
+
 		return "Das Thema '" + thema.getTitel() + "' wurde erfolgreich gespeichert.";
 	}
 	
-	
+
 	
 	public ArrayList<ThemaObject> ladeAlleThemen() {
         ArrayList<ThemaObject> liste = new ArrayList<>();
@@ -29,4 +59,9 @@ public class DataManager {
         return liste;
         }
 
+	
+	private static final String URL ="jdbc:mysql://localhost:3306/examen_db";
+	private static final String USER = "root";
+	private static final String PASSWORD = "";
+	
 }
